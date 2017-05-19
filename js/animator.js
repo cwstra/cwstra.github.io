d7d0d9553725d22b700e27536f15a7378329ac2b
cwstra <cwstra@gmail.com>
2017-05-19 18:15:01 -0400

    Don't reference commented section

--- js/animator.js
+++ js/animator.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'solarSystem', {preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'solarSystem'/*, {preload: preload, create: create, update: update }*/);
 /*slickUI = this.game.plugins.add(Phaser.Plugin.SlickUI);
 
 function preload() {
