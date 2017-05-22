ebe86e8cf959034bd9e2432379fc8b78a2b6b9f0
cwstra <cwstra@gmail.com>
2017-05-21 20:45:13 -0400

    Got rid of header

--- js/index.js
+++ js/index.js
@@ -1754,7 +1754,7 @@ function tableGen(sysname,stars,orbitZones,planets,satel,asteroids,capturedPlane
   		return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 	}
 	
-	var tabstr = '<table>\n\t<th><td colspan="3">'+sysname+" System</td><th>\n";
+	var tabstr = '<table>\n\t<tr><td colspan="3">'+sysname+" System</td><tr>\n";
 	var i,s,img,prop,j; for (i=0;i<stars.length;i++){
 		tabstr += '\t<tr><td colspan="3">';
 		if (i===0){
