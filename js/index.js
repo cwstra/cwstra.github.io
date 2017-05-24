0a897fb7a3e2799017fa32785d6132d2bbb78310
cwstra <cwstra@gmail.com>
2017-05-24 03:39:02 -0400

    Check

--- js/index.js
+++ js/index.js
@@ -1542,7 +1542,7 @@ function zonePop(zones, captureTest) {
 	for (i = 0; i < zones.length; i++) {
 		t = [];
 		for (j = 0; j < zones[i].length; j++) {
-			if (selected.hasOwnProperty(i.toString() + " " + j.toString())) {
+			if (selected.hasOwnProperty(i.toString() + " " + j.toString())&&selected!=={}) {
         console.log(["Aha?",JSON.stringify(selected),i.toString() + " " + j.toString()])
 				t.push(selected[i.toString() + " " + j.toString()]);
 			}
