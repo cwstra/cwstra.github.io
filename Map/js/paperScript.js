490b57b9d61a6868d79cfc53ef804221b60ac6aa
cwstra <cwstra@gmail.com>
2017-06-04 01:53:01 -0400

    Test

--- Map/js/paperScript.js
+++ Map/js/paperScript.js
@@ -9,6 +9,7 @@ var text = new PointText(new Point(view.size.width/2, view.size.height/2));
 text.justification = 'center';
 text.fillColor = 'black';
 text.content = "Testing, 1, 2, ";
+console.log("Over")
 var bbox, diagram;
 var oldSize = view.size;
 var spotColor = new Color('red');
