a5d14acf516e33150bdd04828fe5820174337251
cwstra <cwstra@gmail.com>
2017-06-06 15:28:52 -0400

    Restart

--- Map/js/index.js
+++ Map/js/index.js
@@ -8,7 +8,7 @@ function stage(sta){
 						  '<li>Move on to making a <select id="land"><option value="perlin" selected>"Realistic" land mass</option><option value="radial">Radial land mass</option><option value="noOcean">map with no ocean</option></select>: <button onclick="stage()">Go!</button></li>';
 				break;
 			case "Ocean":
-				hString = '';
+				hString = '<li>Restart <button onclick="restart()">Go!</button></li>';
 		}
 		tools.html(hString);
 	}
@@ -18,5 +18,9 @@ function stage(sta){
 		window.paperNext();
 	}
 	change(mapState);
+}
+
+function restart(){
+	stage('restart');
 }
 $(document).ready(function(){stage("restart");})
